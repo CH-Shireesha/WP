7739abee3a934a78a69a052c5b737fca1277c7e8
CH-Shireesha <shirishach26@msitprogram.net>
2020-04-20 13:50:26 +0530

    ADD: Authentication

--- project1/application.py
+++ project1/application.py
@@ -20,21 +20,21 @@ Session(app)
 db.init_app(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
-Session = scoped_session(sessionmaker(bind=engine))
-session=Session()
+# engine = create_engine(os.getenv("DATABASE_URL"))
+# Session = scoped_session(sessionmaker(bind=engine))
+# session=Session()
 @app.route("/")
 def index():
     return "project 1:TODO"
 @app.route("/register", methods = ['POST', 'GET'])
 def register():
     if request.method =='POST':
-        udata=user(request.form['name'],request.form['password'],request.form['mail'],
+        data=user(request.form['name'],request.form['password'],request.form['mail'],
         request.form['gender'],request.form['age'],request.form['birthday'])
-        userd=user.query.filter_by(mail=request.form['mail']).first()
-        if userd is not None:
+        userdata=user.query.filter_by(mail=request.form['mail']).first()
+        if userdata is not None:
             return render_template("error.html")
-        db.session.add(udata)
+        db.session.add(data)
         db.session.commit()
         print("Sucesssfully Registered")
         return render_template("sucess.html")
@@ -43,12 +43,28 @@ def register():
 
 @app.route('/admins')
 def admin():
-    usersinfo = user.query.all()
-    return render_template("Details.html",register = usersinfo)
+    users = user.query.all()
+    return render_template("Details.html",register = users)
 
+@app.route('/auth',methods=['POST','GET'])
+def auth():
+    if request.method =='POST':
+        email = request.form.get("mail")
+        pwd = request.form.get("password")
+        details = user.query.get(email)
+        if details != None:
+            if pwd == details.password:
+                session['mail'] = email
+                return render_template("Home.html")
+            else:
+                return "Wrong password!!"
+        else:
+            return render_template("registerForm.html", message = "Register to login")
 
-
-
+@app.route('/logout', methods=['POST','GET'])
+def logout():
+    session['mail'] = None
+    return redirect('/register')
 
 
 
