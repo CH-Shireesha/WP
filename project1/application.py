ede652776c971389458632699938e89302116902
BolluHimana <bolluhimana@msitprogram.net>
2020-05-11 00:01:29 +0530

    Done with single page Review!!

--- project1/application.py
+++ project1/application.py
@@ -1,7 +1,7 @@
 import os, requests
 
 from database import *
-from flask import Flask, session,render_template, request,redirect
+from flask import Flask, session,render_template, request,redirect,jsonify
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -102,11 +102,42 @@ def rev(arg):
             return render_template("review.html",data=book,name=name,feedback=feedback,message="Thank you!! for the feedback")
         except:
             db.session.rollback()
-            return render_template("review.html",data=book,message="user has already given review")
+            return render_template("review.html",data=book,name=name,feedback=feedback,message="user has already given review")
     else :              
         return render_template("review.html",data=book,name=name,feedback=feedback)
-
-
+@app.route("/api/Sreview", methods  =['POST'])
+def submitreview():
+    if not request.is_json:
+        message = "Invalid request format"
+        return jsonify(message),400
+    isbn = request.args.get('isbn')
+    try:
+        book = db.session.query(Book).filter(isbn == isbn).first()
+    except:
+        message = "Please Try again Later"
+        return jsonify(message),500
+    if book is None:
+        message = "Please enter valid ISBN"
+        return jsonify(message), 404
+    userName = session.get('name')
+    title = "At Grave's End"
+    rating = request.get_json()['rating']
+    feedback = request.get_json()['feedback']
+    user = review.query.filter_by(userName=userName,title=book.title).first()
+    if user is not None:
+        message = "Sorry you can't review this book again"
+        print(message)
+        return jsonify(message), 409
+    reviewdata=review(userName,title,rating,feedback)
+    try:
+        db.session.add(reviewdata)
+        db.session.commit()
+    except:
+        message = "Please Try Again "
+        return jsonify(message), 500
+    # print(isbn,rating,comment)
+    message = "Review submitted successfully"
+    return jsonify(message), 200
 
 @app.route('/search', methods=['POST','GET'])
 def search():
@@ -119,19 +150,14 @@ def search():
         matchString = "%{}%".format(search)
         print(matchString)
         if searchType == 'isbn':
-            book = DB.query(Book).filter(Book.isbn.like(matchString)).all()
+            book = db.session.query(Book).filter(Book.isbn.like(matchString)).all()
         elif searchType == 'title':
-            book = DB.query(Book).filter(Book.title.like(matchString)).all()
+            book = db.session.query(Book).filter(Book.title.like(matchString)).all()
         elif searchType == 'author':
-            book = DB.query(Book).filter(Book.author.like(matchString)).all()
+            book = db.session.query(Book).filter(Book.author.like(matchString)).all()
         if len(book)== 0:
             message = "No Matching results found!"
         print (book)
         return render_template("Home.html", name=name, books = book, message = message)
 
     return render_template("Home.html", name=name)
-
-
-# @app.route("/book/<string:arg>")
-# def book(arg):
-#     return render_template("Book.html", isbn = arg)
